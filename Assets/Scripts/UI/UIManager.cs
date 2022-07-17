d88eb71dc8a11f0b4e3336a088cfaf9fc92b3862
dantedemon14 <doc1revolution@gmail.com>
2022-07-17 02:56:35 +0200

    Updated UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,8 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : Singleton<UIManager>
 {
+    [Header("Level to Load")]
+    [SerializeField] private string newGameLevel;
+    [SerializeField] private string levelToLoad;
 
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+    public void OnNewGameDialogYes()
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void OnLoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
