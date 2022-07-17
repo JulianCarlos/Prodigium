248d00d0c288b66a31911711eba8582e998e5e07
dantedemon14 <doc1revolution@gmail.com>
2022-07-17 04:18:49 +0200

    Implemented Sound Menu UI, and more pop ups

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : Singleton<UIManager>
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationDialog = null;
+
     [Header("Level to Load")]
     [SerializeField] private string newGameLevel;
     [SerializeField] private string levelToLoad;
@@ -33,5 +42,35 @@ public class UIManager : Singleton<UIManager>
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(nameof(ConfirmationWindow));
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationWindow()
+    {
+        confirmationDialog.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationDialog.SetActive(false);
     }
 }
