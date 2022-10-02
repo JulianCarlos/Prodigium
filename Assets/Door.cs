b6ab3c882118902f0c4a3709a952f43b75df94fd
dantedemon14 <doc1revolution@gmail.com>
2022-10-02 20:59:59 +0200

    Updated Cursor Data

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
-    private Quaternion closedDoorRotation;
+    private Vector3 closedDoorRotation;
+    private Vector3 openDoorRotation;
 
     private bool isInUse = false;
     private bool isOpen = false;
 
     private void Start()
     {
-        closedDoorRotation = transform.localRotation;
+        closedDoorRotation = transform.localRotation.eulerAngles;
     }
 
     public void Use(Player player)
@@ -27,20 +28,24 @@ public class Door : MonoBehaviour, IInteractable
         isInUse = true;
 
         if (isOpen)
-            StartCoroutine(nameof(OpenDoor));
-        else if (!isOpen)
             StartCoroutine(nameof(CloseDoor));
+        else if (!isOpen)
+            StartCoroutine(nameof(OpenDoor));
     }
 
     private IEnumerator OpenDoor()
     {
+        Debug.Log("open door");
         yield return null;
         isOpen = true;
+        isInUse=false;
     }
 
     private IEnumerator CloseDoor()
     {
+        Debug.Log("close door");
         yield return null;
         isOpen = false;
+        isInUse = false;
     }
 }
